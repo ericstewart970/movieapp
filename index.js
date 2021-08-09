c2d1374e049b8f2a1203205f875febeca0e3d57c
ericstewart970 <76798120+ericstewart970@users.noreply.github.com>
2021-08-08 21:41:27 -0600

    called new users, delete,favorite movie delete favorite movie

--- index.js
+++ index.js
@@ -8,71 +8,71 @@ let topTen = [
   //1
   {
     title: 'Happy Gilmore',
-    Director:'',
-    Genra:''
+    Director:'Dennis Dugan',
+    Genra:'Sports Comedy'
   },
 
   //2
   {
     title: 'Billy Madison',
-    Director:'',
-    Genra:''
+    Director:'Tamra Davis',
+    Genra:'Comedy'
   },
 
   //3
   {
     title: '8 Mile',
-    Director:'',
-    Genra:''
+    Director:'Curtis Hanson',
+    Genra:'Musical Drama'
   },
 
   //4
   {
     title: 'Get Rich or Die Tryin',
-    Director:'',
-    Genra:''
+    Director:'Jim Sheridan',
+    Genra:'Amercan Hip Hop Crime Drama'
   },
 
   //5
   {
     title: 'The Wolf of Wall Street',
-    Director:'',
-    Genra:''
+    Director:'Martin Scorsese',
+    Genra:'Comedy Crime'
   },
 
   //6
   {
     title: 'Aviator',
-    Director:'',
-    Genra:''
+    Director:'Martin Scorsese',
+    Genra:'Drama Film'
   },
 
   //7
   {
     title: 'The Fast and the Furious',
-    Director:'',
-    Genra:''
+    Director:'Rob Cohen',
+    Genra:'Action Film'
   },
 
   //8
   {
     title: 'Caddyshack',
-    Director:'',
-    Genra:''
+    Director:'Harold Ramis',
+    Genra:'Sports Comedy'
   },
 
   //9
   {
     title: 'Blazing Saddles',
-    Director:'',
-    Genra:''
+    Director:'Mel Brooks',
+    Genra:'Western Comedy'
   },
 
   //10
   {
     title: 'American Pie',
-    Director:'',
-    Genra:''
+    Director:'Paul Weitz',
+    Genra:'Comedy'
   },
 ];
 
@@ -92,7 +92,56 @@ app.get('/movies', (req, res) => {
 // gets single movie info
 app.get('/movies/:title', (req, res) => {
   res.json(topTen.find( (movie) =>
-    { return topTen.title === req.params.title }));
+    { return movie.title === req.params.title }));
+});
+
+// gets director info
+app.get('/movies/:director', (req, res) => {
+  res.json(director.find( (director) =>
+    { return director.name === req.params.name}));
+
+// gets genra info
+app.get('/movies/:genra', (req, res) => {
+  res.json(genra.find( (movie) =>
+    { return genra.name=== req.params.name}));
+
+// Add new user (/users) to post
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Update username (/users/:user/:userid)
+app.put('/users/:user/:userid', (req, res) => {
+  let user = users.find((user) => {return user.name === req.params.name});
+})
+
+// Add movie to users favorite list
+app.post('/users/:user/:movies/:favMovie', (req, res) => {
+  res.send('You favorite movies not yet able to be add but will be coming soon!');
+});
+
+// deleting favorite movies (/users/:user/:movies/remove/:favMovie)
+app.delete('/users/:user/:movies/remove/:favMovie', (req, res) => {
+  res.send('Sorry no movie to delete yet but hold tight!');
+});
+
+// Delete Users
+app.delete('/users/:id', (req, res) => {
+  let user = users.find((user) => {return user.id === req.params.id });
+
+  if (user) {
+    users = users.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('User ' + req.params.id + ' was deleted.');
+  }
 });
 
 app.use((err, req, res, next) => {
